63ac3ac0e583ec4e8bd650ae71056c528aa4dabc
Hudson Menezes <hudson.bm23@gmail.com>
2023-04-05 16:20:43 -0300

    new project

--- src/data/data.js
+++ src/data/data.js
@@ -5,24 +5,32 @@ import RelogioDigital from "../assets/projects/RelogioDigital.png";
 import TodoList from "../assets/projects/TodoList.png";
 import Homeland from "../assets/projects/Homeland.png";
 import LawFirm from "../assets/projects/LawFirm.png";
+import PetGlamour from "../assets/projects/PetGlamour.png";
 
 export const data = [
   {
     id: 1,
+    name: "Pet Glamour",
+    image: PetGlamour,
+    github: "https://github.com/HudsonMenezes/petglamour",
+    live: "https://petglamour.vercel.app/",
+  },
+  {
+    id: 2,
     name: "HomeLand",
     image: Homeland,
     github: "https://github.com/HudsonMenezes/real-estate-react-tailwind",
     live: "https://homeland-house.vercel.app/",
   },
   {
-    id: 2,
+    id: 3,
     name: "LawFirm",
     image: LawFirm,
     github: "https://github.com/HudsonMenezes/law-firm",
     live: "https://law-firm-reacttailwind.vercel.app/",
   },
   {
-    id: 3,
+    id: 4,
     name: "Suculenta & Cia",
     image: SuculentaCia,
     github: "https://github.com/HudsonMenezes/suculentaecia-ecommerce",
@@ -30,18 +38,11 @@ export const data = [
   },
 
   {
-    id: 4,
+    id: 5,
     name: "Flowers By Rose",
     image: FlowersByRose,
     github: "https://github.com/HudsonMenezes/pagina-htmlcss-flowersbyrose",
     live: "https://hudsonmenezes.github.io/pagina-htmlcss-flowersbyrose/",
-  },
-  {
-    id: 5,
-    name: "Relógio Digital",
-    image: RelogioDigital,
-    github: "https://github.com/HudsonMenezes/relogio_digital",
-    live: "https://hudsonmenezes.github.io/relogio_digital/",
   },
   {
     id: 6,
